78ecf7fc5e3a1d90114d689433a8e630f79dc12f
Mohamed-Mayallo <reckoning58@gmail.com>
2019-08-24 02:40:28 +0200

    Interpolation

--- simple-vue.js
+++ simple-vue.js
@@ -1,12 +1,30 @@
 class SimpleVue {
-  constructor(dataObj) {
+  constructor($options) {
     // 1- Initiate dependencies object
     this.deps = {};
 
-    this.data = dataObj;
+    this.data = $options.data;
+    this.computed = $options.computed;
+
+    this.reactiveTags = [];
 
     // 2- Convert data object to reactive properties
     this.convertDateToReactiveProperties(this.data);
+
+    // Wrap constructor accessed property with proxy [ACCESS MODIFIER REPLACEMENT]
+    return new Proxy(this, {
+      get(instance, property) {
+        if (property in instance) return instance[property];
+        return (
+          instance.data[property] ||
+          instance.computed[property] ||
+          new Error('This property does not exist')
+        );
+      },
+      set(target, prop, value) {
+        console.log(target, prop, value, 'MAY BE USEFUL ....');
+      }
+    });
   }
 
   convertDateToReactiveProperties(data) {
@@ -42,16 +60,72 @@ class SimpleVue {
       Array.from(nodes).map(node => {
         // Get property name "title"
         let propName = node.attributes['v-model'].value;
+        this.setReactiveTags(reactiveDataObj, propName);
 
-        // To set initial value
-        node.textContent = reactiveDataObj[propName];
+        node.value = reactiveDataObj[propName];
+        node.addEventListener('input', event => {
+          reactiveDataObj[propName] = event.target.value;
+
+          this.updateReactiveTagsValues(reactiveDataObj);
+        });
+      });
+  }
+
+  setReactiveTags(reactiveDataObj, property) {
+    let boundedElements = [
+      ...this.loopOverNodesToBindProps(document.body, property)
+    ];
+
+    Array.from(boundedElements).map(n => {
+      // To set initial value
+      n.textContent = reactiveDataObj[property];
+      let dataAttribute = `data-${property}`;
+      let dataAttributeValue = Math.floor(Math.random(5) * 123456);
+      n.setAttribute(dataAttribute, dataAttributeValue);
+
+      this.reactiveTags.push({
+        data: property,
+        value: dataAttributeValue
+      });
+
+      // Callback that will be attached to the reactive property
+      let dep = () => (n.textContent = reactiveDataObj[property]);
+
+      // 4- Attach dependencies to the reactive property
+      this.setReactivePropertyDeps(property, dep);
+    });
+  }
+
+  updateReactiveTagsValues(reactiveDataObj) {
+    this.reactiveTags.map(tag => {
+      let nodes = document.querySelectorAll(
+        `[data-${tag.data.toLowerCase()}="${tag.value}"]`
+      );
+
+      Array.from(nodes).map(node => {
+        node.textContent = reactiveDataObj[tag.data];
 
         // Callback that will be attached to the reactive property
-        let dep = () => (node.textContent = reactiveDataObj[propName]);
+        let dep = () => (node.textContent = reactiveDataObj[tag.data]);
 
         // 4- Attach dependencies to the reactive property
-        this.setReactivePropertyDeps(propName, dep);
+        this.setReactivePropertyDeps(tag.data, dep);
       });
+    });
+  }
+
+  loopOverNodesToBindProps(parent, propName) {
+    let children = parent.children;
+    let regex = new RegExp(`\\s*{*\\s*{*\\s*${propName}\\s*}*\\s*}*\\s*`);
+
+    let boundedElements = [];
+    if (children && children.length) {
+      // Get elements contain the same prop name
+      Array.from(children).map(child => {
+        if (regex.test(child.textContent)) boundedElements.push(child);
+      });
+    }
+    return boundedElements;
   }
 
   setReactivePropertyDeps(propName, dep) {
@@ -62,7 +136,15 @@ class SimpleVue {
 }
 
 let simpleVue = new SimpleVue({
-  title: 'Hello all'
+  data: {
+    fName: 'Mohamed',
+    lName: 'Mayallo'
+  },
+  computed: {
+    fullName() {
+      return `${this.fName} ${this.lName}`;
+    }
+  }
 });
 
 function updateNode(prop, event) {
